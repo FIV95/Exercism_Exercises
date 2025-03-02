5e4fa44d1398482f675f1daf5f7689c44ae541ae
fiv95 <frank4law@gmail.com>
2025-03-01 22:52:44 -0500

    initial planning for grain exercise

---
+++ rust/grains/src/lib.rs
@@ -0,0 +1,38 @@
+pub fn square(s: u32) -> u64 {
+    todo!("grains of rice on square {s}");
+}
+
+pub fn total() -> u64 {
+    todo!();
+}
+
+/*
+ * ## Instructions
+
+Calculate the number of grains of wheat on a chessboard.
+
+A chessboard has 64 squares.
+Square 1 has one grain, square 2 has two grains, square 3 has four grains, and so on, doubling each time.
+
+Write code that calculates:
+
+- the number of grains on a given square
+- the total number of grains on the chessboard
+
+// 64 boards
+// maximum number of grains:
+// Square || Grain
+//  1           1
+//  2           2
+//  3           4
+//  4           8
+//  5           16
+//  6           32
+//  7           64
+//  8           128
+//  9           256
+//  10          512
+//
+//  so with 64 max tiles we can have a max number of grains: 
+//
+*/
